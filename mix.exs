502869368dfd69ed39bfc276f847038a40adbf9a
AJ <aj@thecampustap.com>
2018-01-04 13:01:02 -0800

    ...

--- mix.exs
+++ mix.exs
@@ -20,7 +20,7 @@ defmodule EsprezzoCore.Mixfile do
   def application do
     [
       mod: {EsprezzoCore.Application, []},
-      extra_applications: [:logger, :runtime_tools]
+      extra_applications: [:logger, :runtime_tools, :edeliver],
     ]
   end
 
