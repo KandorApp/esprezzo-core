18a890d25f9957ce9c21cc6a2940b9f9bd688e2b
AJ <aj@thecampustap.com>
2018-01-09 17:23:25 -0800

    ...

---
+++ scripts/install_deps_ubuntu.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# Install Erlang and Elixir
+wget https://packages.erlang-solutions.com/erlang-solutions_1.0_all.deb && sudo dpkg -i erlang-solutions_1.0_all.deb;
+sudo apt-get update;
+sudo apt-get install esl-erlang;
+sudo apt-get install elixir;
+
+# Install postgeres
+sudo apt-get install postgresql;
+
