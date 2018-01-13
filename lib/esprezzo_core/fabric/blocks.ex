4c0f4ea0ef9351d9c5850ab5b6b520a4121fcb3d
AJ <aj@thecampustap.com>
2018-01-12 17:17:25 -0800

    ...

--- lib/esprezzo_core/fabric/blocks.ex
+++ lib/esprezzo_core/fabric/blocks.ex
@@ -1,5 +1,13 @@
 defmodule EsprezzoCore.Fabric.Blocks do
   
+alias EsprezzoCore.Fabric.Types.Block
 
-  
+
+  @spec build_hash(t) :: String.t()
+  def build_hash(%Block{index: i, previous_hash: h, timestamp: ts, data: data, nonce: n}) do
+    "#{i}#{h}#{ts}#{BlockData.hash(data)}#{n}"
+    |> Crypto.hash(:sha256)
+    |> Base.encode16()
+  end
+
 end
