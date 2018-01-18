67d8412c6da086133ab7749a812478d2139dc2d0
AJ <aj@thecampustap.com>
2018-01-18 11:31:58 -0800

    restructure

---
+++ lib/esprezzo_core/peer_net/lib/client.ex
@@ -0,0 +1,44 @@
+defmodule EsprezzoCore.PeerNet.Client do
+  require Logger
+  require IEx
+  alias EsprezzoCore.PeerNet.StateTracker
+  alias EsprezzoCore.PeerNet.NetMeta
+
+  @doc"""
+  EsprezzoCore.PeerNet.Client.connect("127.0.0.1", 19876)
+  """
+  def connect(address, port) do
+    [a,b,c,d] = String.split(address, ".")
+      |> Enum.map(fn x -> 
+          {n, _} = Integer.parse(x)
+          n
+        end )
+    opts = [:binary, :inet, active: false, packet: :line]
+    # with {:ok, socket} = :gen_tcp.connect({a, b, c, d}, port, opts) do
+    #   {:ok, pid} = StateTracker.add_peer(socket, :gen_tcp)
+    #   :gen_tcp.controlling_process(socket, pid)
+    #   {:ok, pid}
+    # end
+    case :gen_tcp.connect({a, b, c, d}, port, opts) do
+      {:ok, socket} ->  
+        {:ok, pid} = StateTracker.add_peer(socket, :gen_tcp)
+        :gen_tcp.controlling_process(socket, pid)
+      {:error, reason} ->
+        Logger.error( fn ->
+          "Could not connect to remote host"
+        end)
+        {:error, reason}
+    end
+
+  end
+
+  def connect_to_bootstrap_nodes do
+    NetMeta.bootstrap_nodes
+      |> Enum.map(fn x ->
+        [ip, port] = String.split(x, ":")
+        {port, _} = Integer.parse(port)
+        __MODULE__.connect(ip, port)
+    end)
+  end
+
+end
