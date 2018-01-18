c0f234d655c04c50b449132969df891804bc173d
AJ <aj@thecampustap.com>
2018-01-18 11:25:49 -0800

    ...

--- lib/esprezzo_core/command/command.ex
+++ lib/esprezzo_core/command/command.ex
@@ -1,13 +1,14 @@
 defmodule EsprezzoCore.Command do
   use GenServer
   alias EsprezzoCore.PeerNet
-  
+
   def start_link(opts) do
     GenServer.start_link(__MODULE__, opts, name: __MODULE__)
   end
-
+ 
   @doc"""
   Initialize Command Worker
+  callbacks
   """
   def init(opts) do
     PeerNet.bootstrap_connections()
