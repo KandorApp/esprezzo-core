75e240ffb7708a319f734c2cfcbe704260e4bf9c
AJ <aj@thecampustap.com>
2018-01-18 14:53:38 -0800

    ...

--- lib/esprezzo_core/peer_net/lib/tcp_handler.ex
+++ lib/esprezzo_core/peer_net/lib/tcp_handler.ex
@@ -9,8 +9,8 @@ defmodule EsprezzoCore.PeerNet.TCPHandler do
          
   def init(ref, socket, transport, _Opts = []) do
     :ok = :ranch.accept_ack(ref)
-    tcp_options = [:binary, {:packet, 4}, active: false, reuseaddr: true]
-    :ok = transport.setopts(socket, [{:active, true}])
+    tcp_options = [:binary, {:packet, 4}, active: true, reuseaddr: true]
+    :ok = transport.setopts(socket, tcp_options)
     {:ok, pid} = StateTracker.add_peer(socket, transport)
     :ranch_tcp.controlling_process(socket, pid)
     #loop(socket, transport)
