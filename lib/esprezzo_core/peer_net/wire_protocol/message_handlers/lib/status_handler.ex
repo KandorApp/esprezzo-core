671bc3ec5cc70653a6bc05efdb5d65e09b5ab563
AJ <aj@thecampustap.com>
2018-02-06 16:15:46 -0800

    ...

--- lib/esprezzo_core/peer_net/wire_protocol/message_handlers/lib/status_handler.ex
+++ lib/esprezzo_core/peer_net/wire_protocol/message_handlers/lib/status_handler.ex
@@ -1,6 +1,8 @@
 defmodule EsprezzoCore.PeerNet.WireProtocol.StatusHandler do
   require Logger
   require IEx
+  alias EsprezzoCore.Blockchain
+
 
 
   @doc """
@@ -12,7 +14,13 @@ defmodule EsprezzoCore.PeerNet.WireProtocol.StatusHandler do
   """
   def process(command) do
     inspect(command)
-    IEx.pry
+    case command.block_height >= Blockchain.current_height() do
+      true -> 
+        # ask for blocks
+        IEx.pry
+      false ->
+        false
+    end
   end
 
 end
