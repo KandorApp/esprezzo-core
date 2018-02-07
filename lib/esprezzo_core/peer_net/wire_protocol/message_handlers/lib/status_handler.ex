b3c727796de00123c418f09baa4154d5d0c17234
AJ <aj@thecampustap.com>
2018-02-06 16:02:19 -0800

    ...

---
+++ lib/esprezzo_core/peer_net/wire_protocol/message_handlers/lib/status_handler.ex
@@ -0,0 +1,16 @@
+defmodule EsprezzoCore.PeerNet.WireProtocol.StatusHandler do
+  require Logger
+  require IEx
+
+
+  @doc """
+  Is the genesis hash valid?
+
+  Does the sender have a higher block count that I do?
+
+  """
+  def process(command) do
+    IEx.pry
+  end
+
+end
