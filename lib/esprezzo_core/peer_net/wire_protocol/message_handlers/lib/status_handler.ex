6b88b21edda4177e101e7819cca315ac852b4de9
AJ <aj@thecampustap.com>
2018-02-06 20:03:10 -0800

    ...

--- lib/esprezzo_core/peer_net/wire_protocol/message_handlers/lib/status_handler.ex
+++ lib/esprezzo_core/peer_net/wire_protocol/message_handlers/lib/status_handler.ex
@@ -18,6 +18,7 @@ defmodule EsprezzoCore.PeerNet.WireProtocol.StatusHandler do
     case command.block_height >= Blockchain.current_height() do
       true -> 
         # ask for blocks
+        IEx.pry
         {:ok, Commands.build("REQUEST_BLOCKS", Blockchain.current_height())}
       false ->
         Logger.warn("SYNC_COMPLETE")
